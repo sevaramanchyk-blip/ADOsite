05022d6c4095f6231615d77820aa118169ca059e
AlexeyZabrodsky <LeshaZabrodsky@gmail.com>
2026-06-02 22:01:03 +0300

    Мои ссылки

---
+++ Tests/API/test_status_code.py
@@ -0,0 +1,23 @@
+import requests
+import pytest
+import allure
+
+@allure.feature('Тесты API')
+@allure.story('Проверка статус кода')
+class TestApiAdoSite():
+    @allure.title('Проверка статус кода')
+
+    @pytest.mark.parametrize('status_code, name_site, url', [
+        (200, 'https://ado-shop.com/', "https://web.telegram.org/a/#8656139643"),
+        (200, 'https://ado-shop.com/', "https://ado-shop.com/collections/all-merch"),
+        (200, 'https://ado-shop.com/', "https://ado-shop.com/collections/shinzou"),
+        (200, 'https://ado-shop.com/', "https://ado-shop.com/collections/2nd-original-album-zanmu"),
+        (200, 'https://ado-shop.com/', "https://ado-shop.com/collections/hibana"),
+        (200, 'https://ado-shop.com/', "https://ado-shop.com/collections/ados-best-adobum"),
+        (200, 'https://ado-shop.com/', "https://ado-shop.com/collections/phantom-siita"),
+    ])
+    def test_api_ado_site(self, status_code, name_site, url):
+        with allure.step('Вызов ручки'):
+            response = requests.get(url=url)
+        with allure.step(f'Проверка статус кода страницы {name_site}'):
+            assert response.status_code == status_code
