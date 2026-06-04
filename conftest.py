93a4ea08bc211d9957dfae1ea54e4d55eb1e15d8
AlexeyZabrodsky <LeshaZabrodsky@gmail.com>
2026-06-04 21:53:01 +0300

    Настройки

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import allure
+
+
+@pytest.fixture
+def driver():
+    chrome_options = Options()
+    chrome_options.add_argument('--start-maximized')
+    # chrome_options.add_argument('--headless')
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.implicitly_wait(10)
+
+    yield driver
+    driver.quit()
