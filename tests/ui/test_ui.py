c5c6fe86d1a8bea70ba58eed5913431adf5673a3
AlexeyZabrodsky <LeshaZabrodsky@gmail.com>
2026-06-04 21:53:54 +0300

    Тест ui

---
+++ tests/ui/test_ui.py
@@ -0,0 +1,17 @@
+import allure
+from selenium.webdriver.common.by import By
+from conftest import driver
+
+
+def test_links5(driver):
+    driver.get('https://ado-shop.com/')
+    elements = [
+        (driver.find_element(By.ID, "HeaderMenu-home"), 'Кнопка Home'),
+        (driver.find_element(By.ID, "HeaderMenu-music"), 'Кнопка Music'),
+        (driver.find_element(By.ID, "HeaderMenu-merch"), 'Кнопка Merch'),
+        (driver.find_element(By.ID, "HeaderMenu-help"), 'Кнопка Help'),
+        (driver.find_element(By.ID, "HeaderMenu-contact"), 'Кнопка Contact')
+    ]
+    for element, text_element in elements:
+        with allure.step(f'Проверка кликабельно {text_element}'):
+            assert element.is_enabled(), f'{text_element} не кликабельна'
